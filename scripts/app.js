135cffdd1a195b3369349c4cddcae1deb8b068ea
StephensGit <stephenmoran@live.ie>
2021-08-03 08:50:03 +0100

    Added function to generate random password and display it

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
-console.log("Test");
+// Get a hold of the input element to display the generated code
+// Get a hold of the button element to invoke the function to generate a new password
+
+let passwordInput = document.getElementById('password-input');
+let generateBtn = document.getElementById('generate');
+
+generateBtn.addEventListener('click', () => {
+  // set chars
+  let chars =
+    '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // This will eventually change with a slider input
+  let passwordLength = 12;
+  let password = '';
+  for (let i = 0; i <= passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    console.log(randomNumber);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  passwordInput.value = password;
+});
