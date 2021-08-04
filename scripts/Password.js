af698a80a4203573e4593e6e9e11f45949db6849
StephensGit <stephenmoran@live.ie>
2021-08-04 18:50:41 +0100

    Updated code to use JS es6 Classes

--- scripts/Password.js
+++ scripts/Password.js
@@ -1,9 +1,15 @@
 class Password {
-  constructor(pass) {
-    this.pass = pass;
+  constructor(length) {
+    this.length = length;
+    this.password = '';
+    this.chars =
+      '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  //
-  calcArea() {
-    return this.height * this.width;
+  generatePassword() {
+    for (let i = 0; i <= this.length; i++) {
+      let randomNumber = Math.floor(Math.random() * this.chars.length);
+      this.password += this.chars.substring(randomNumber, randomNumber + 1);
+    }
+    return this.password;
   }
 }
